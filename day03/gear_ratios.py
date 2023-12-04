9e52447b68c212b16032a02619e74d881f4bd723
Yacine <yadlazik@yahoo.fr>
2023-12-04 21:24:24 +0100

    day03 tried but blocked

--- day03/gear_ratios.py
+++ day03/gear_ratios.py
@@ -39,73 +39,94 @@ def create_engine(text):
     
     return engine
 
-def spot_symbol(engine, y, x, nb, begin = True):
-    # min_limit_x = x - 1 >= 0
-    # min_limit_y = y - 1 >= 0
-    # max_limit_x = x + 1 < len(engine[y])
-    # max_limit_y = y + 1 < len(engine)
-
-    # # droite
-    # if max_limit_x :
-    #     if engine[y][x + 1].isnumeric():
-    #         return spot_symbol(engine, y, x + 1, nb + engine[y][x + 1], False)
-    #     if engine[y][x + 1] != '.':
-    #         # print(nb)
-    #         return int(nb)
-
-    # # gauche
-    # if begin and min_limit_x and engine[y][x - 1] != '.' and not engine[y][x - 1].isnumeric():
-    #     # print(nb)
-    #     return int(nb)
-
-    # # haut gauche
-    # if min_limit_y and min_limit_x and engine[y - 1][x - 1] != '.' and not engine[y - 1][x - 1].isnumeric():
-    #     # print(nb)
-    #     return int(nb)
-
-    # # bas gauche
-    # if max_limit_y and min_limit_x and engine[y + 1][x - 1] != '.' and not engine[y + 1][x - 1].isnumeric():
-    #     # print(nb)
-    #     return int(nb)
-
-    # # haut
-    # if min_limit_y and engine[y - 1][x] != '.' and not engine[y - 1][x].isnumeric():
-    #     # print(nb)
-    #     return int(nb)
-
-    # # bas
-    # if max_limit_y and engine[y + 1][x] != '.' and not engine[y + 1][x].isnumeric():
-    #     # print(nb)
-    #     return int(nb)
-
-    # # haut droite
-    # if min_limit_y and max_limit_x and engine[y - 1][x + 1] != '.' and not engine[y - 1][x + 1].isnumeric():
-    #     # print(nb)
-    #     return int(nb)
-
-    # # bas droite
-    # if max_limit_y and max_limit_x and engine[y + 1][x + 1] != '.' and not engine[y + 1][x + 1].isnumeric():
-    #     # print(nb)
-    #     return int(nb)
-
-    # return 0
-
-    return 0
-
-def browse_engine(engine):
-    sum = 0
-    tmp = 0
+def browse_number(engine, y, x):
     nb = ""
+    nb_pos = x
 
+    while nb_pos < len(engine[y]) and engine[y][nb_pos].isnumeric():
+        nb += engine[y][nb_pos]
+        nb_pos += 1
+    
+    return [int(nb), nb_pos - 1]
+
+def spot_symbol(engine, y, x):
+    min_limit_x = x - 1 >= 0
+    min_limit_y = y - 1 >= 0
+    max_limit_y = y + 1 < len(engine)
+    pos_x = x
+
+    while engine[y][pos_x].isnumeric() and pos_x < len(engine[y]) :
+        max_limit_x = pos_x + 1 < len(engine[y])
+
+        # droite
+        if max_limit_x and engine[y][pos_x + 1] != '.' and not engine[y][pos_x + 1].isnumeric():
+            return True
+
+        # gauche
+        if pos_x == x and min_limit_x and engine[y][pos_x - 1] != '.' and not engine[y][pos_x - 1].isnumeric():
+            return True
+
+        # haut gauche
+        if min_limit_y and min_limit_x and engine[y - 1][pos_x - 1] != '.' and not engine[y - 1][pos_x - 1].isnumeric():
+            return True
+
+        # bas gauche
+        if max_limit_y and min_limit_x and engine[y + 1][pos_x - 1] != '.' and not engine[y + 1][pos_x - 1].isnumeric():
+            return True
+
+        # haut
+        if min_limit_y and engine[y - 1][pos_x] != '.' and not engine[y - 1][pos_x].isnumeric():
+            return True
+
+        # bas
+        if max_limit_y and engine[y + 1][pos_x] != '.' and not engine[y + 1][pos_x].isnumeric():
+            return True
+
+        # haut droite
+        if min_limit_y and max_limit_x and engine[y - 1][pos_x + 1] != '.' and not engine[y - 1][pos_x + 1].isnumeric():
+            return True
+
+        # bas droite
+        if max_limit_y and max_limit_x and engine[y + 1][pos_x + 1] != '.' and not engine[y + 1][pos_x + 1].isnumeric():
+            return True
+        
+        pos_x += 1
+
+    return False
+
+def browse_engine_without_spoting_symbol(engine):
+    sum = 0
+    nb = list()
     line = 0
+
     for col in range(len(engine)):
         while line < len(engine[col]):
             if engine[col][line].isnumeric():
-                print(engine[col][line])
-                tmp = spot_symbol(engine, col, line, nb.join(engine[col][line]))
-                nb = ""
-                line += len(str(tmp)) - 1
-                sum += tmp
+                nb = browse_number(engine, col, line)
+                sum += nb[0]
+                line = nb[1]
+            line += 1
+        line = 0
+    return sum
+
+def browse_engine(engine):
+    sum = 0
+    nb = list()
+    line = 0
+
+    for col in range(len(engine)):
+        while line < len(engine[col]):
+            if engine[col][line].isnumeric():
+                nb = browse_number(engine, col, line)
+                if spot_symbol(engine, col, line):
+                    # if col == 12:
+                    #     print(nb[0])
+                    sum += nb[0]
+                line = nb[1]
+                # if col == 12:
+                #     print(nb)
+                #     print(line)
+                #     print(engine[col][line])
             line += 1
         line = 0
     return sum
@@ -114,6 +135,7 @@ test = "test.txt"
 yacine = "data_yacine.txt"
 paul = "data_paul.txt"
 
-engine = create_engine(test)
+engine = create_engine(yacine)
 
-print(browse_engine(engine))
+print("total :", browse_engine(engine))
+print("total :", browse_engine_without_spoting_symbol(engine))
