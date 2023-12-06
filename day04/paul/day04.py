d68b38c985d56d675b6a465226b2584fc692bacf
Paul Le Bras <paul.le-bras@epitech.eu>
2023-12-06 22:14:06 +0100

    day04: paul - end

--- day04/paul/day04.py
+++ day04/paul/day04.py
@@ -1,5 +1,3 @@
-import re
-
 def read_file_and_strip_newlines(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -7,21 +5,71 @@ def read_file_and_strip_newlines(file_path):
     return lines
 
 
-def split_lines(lines):
+def parse_lines(lines):
+    parsed_lines = []
     for line in lines:
-        return line.split(' | ')
+        splitted_line = line.split(' | ')
+        parsed_lines.append((splitted_line[0].split(' '), splitted_line[1].split(' ')))
+    return parsed_lines
+
+
+def calculate_part1(parsed_lines):
+    total_sum = 0
+
+    for line in parsed_lines:
+        match_count = 0
+        owned_numbers = line[1]
+
+        for winning_number in line[0]:
+            match_count += owned_numbers.count(winning_number)
+        total_sum += int(2**(match_count - 1))
+
+    return total_sum
+
+
+def create_new_array(parsed_lines):
+    new_array = []
+
+    for line in parsed_lines:
+        match_count = 0
+        winning_numbers = line[0]
+        owned_numbers = line[1]
+
+        for winning_number in winning_numbers:
+            match_count += owned_numbers.count(winning_number)
+        new_array.append([match_count, 1])
+
+    return new_array
+
+
+def calculate_part2(matches_and_copies):
+    sum = len(matches_and_copies)
+    for current_index, element in enumerate(matches_and_copies):
+        current_matches = element[0]
+        current_copies = element[1]
+        sum += current_matches * current_copies
+        for n in range(current_index + 1, current_index + current_matches + 1):
+            if n < len(matches_and_copies):
+                matches_and_copies[n][1] += current_copies
+
+    return sum
+
 
 def main(path):
     lines = read_file_and_strip_newlines(path)
-    splitted_lines = split_lines(lines)
+    parsed_lines = parse_lines(lines)
+    part1 = calculate_part1(parsed_lines)
 
-    return splitted_lines
+    matches_and_copies = create_new_array(parsed_lines)
+    part2 = calculate_part2(matches_and_copies)
+
+    return part1, part2
 
 
 if __name__ == "__main__":
-    # file_path = './day03_paul/reduced_sample_paul.txt'
-    # file_path = './day03_paul/sample_paul.txt'
     paul = './day04/paul/data.txt'
+    example = './day04/paul/example.txt'
     result = main(paul)
-    print('part 1 = ', result)
 
+    print('part 1 = ', result[0]) # 28250
+    print('part 2 = ', result[1]) # ?????
